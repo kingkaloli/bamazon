6eb9d32bb819d28e85b919b6399ffc3b4fae7958
Kaloli <rvellark@gmail.com>
2019-02-16 14:37:59 -0700

    update with working loop and questions

--- bamazon.js
+++ bamazon.js
@@ -2,48 +2,56 @@ var mysql = require("mysql");
 var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
- host: "localhost",
+    host: "localhost",
 
- // Your port; if not 3306
- port: 3306,
+    // port
+    port: 3306,
 
- // Your username
- user: "root",
+    // username
+    user: "root",
 
- // Your password
- password: "rootLola",
- database: "bamazonDB"
-});
-connection.connect(function(err) {
-   if (err) throw err;
-
+    // password
+    password: "rootLola",
+    database: "bamazonDB"
 });
 
- connection.query('SELECT * FROM products', (err,rows) => {
-    if(err) throw err;
-    start();
-   
-    console.log(rows);
-   
+connection.connect(function (err) {
+    if (err) throw err;
+    purchaseTable()
  
-  });
 
-  function start() {
-    // prompt for info about the item being put up for auction
-    inquirer
-      .prompt([
-        {
-          name: "item",
-          type: "input",
-          message: "ID of product you want to buy?"
-        },
-        {
-          name: "category",
-          type: "input",
-          message: "How many would you like?"
-        },
-        
-      ])
-    }
-    start();
- 
+})
+var purchaseTable = function() {
+connection.query("SELECT * FROM products", function(err,res){
+for(var i=0; i<res.length; i++){
+    console.log(res[i].itemid+"  "+res[i].productname+"||"+
+    res[i].departmentname+"||"+res[i].price+"||"+res[i].
+    stockquanity+"\n");
+    
+}
+customerOrder(res);
+})
+
+
+ }
+
+var customerOrder = function (res){
+inquirer
+       .prompt([{
+               name: "item",
+               type: "input",
+               message: "ID of product you want to buy?"
+           },
+         {
+                name: "howMany",
+                type: "input",
+                message: "How many would you like?"
+                
+          },
+
+        ])
+ }
+
+
+
+// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
