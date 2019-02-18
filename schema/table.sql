e76fe46ccc83cc472b3f41d231e710980707b04d
Kaloli <rvellark@gmail.com>
2019-02-17 20:24:29 -0700

    update with table

---
+++ schema/table.sql
@@ -0,0 +1,9 @@
+INSERT INTO products(ProductName,DepartmentName,Price,StockQuanity)
+VALUES ("Climbing Rope","Rock Climbing", 120, 8),
+("Trad Pack","Rock Climbing", 200, 3),
+("Harness","Rock Climbing", 60, 5),
+("Tent","Camping", 300, 5),
+("Sleeping Bag","Camping", 400, 2),
+("Trail Shoes","Running", 120, 3),
+("Beer","Drink", 10, 3);
+
