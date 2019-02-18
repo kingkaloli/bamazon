bde93a8cbdb9109df730bca1011de2e23a36a6ca
Kaloli <rvellark@gmail.com>
2019-02-17 20:19:55 -0700

    Update connections still not working

--- bamazon.js
+++ bamazon.js
@@ -2,72 +2,83 @@ var mysql = require("mysql");
 var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
-    host: "localhost",
+  host: "localhost",
 
-    // port
-    port: 3306,
+  // port
+  port: 3306,
 
-    // username
-    user: "root",
+  // username
+  user: "root",
 
-    // password
-    password: "rootLola",
-    database: "bamazonDB"
-});
+  // password
+  password: "rootLola",
+  database: "bamazonDB"
+})
 
 connection.connect(function (err) {
-    if (err) throw err;
-    purchaseTable()
- 
+  if (err) throw err;
+  purchaseTable()
+
 
 })
-var purchaseTable = function() {
-connection.query("SELECT * FROM products", function(err,res){
-for(var i=0; i<res.length; i++){
-    console.log(res[i].itemid+"  "+res[i].productname+"||"+
-    res[i].departmentname+"||"+res[i].price+"||"+res[i].
-    stockquanity+"\n");
-    
+var purchaseTable = function () {
+  connection.query("SELECT * FROM products", function (err, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].itemid + "  " + res[i].productname + "||" +
+        res[i].departmentname + "||" + res[i].price + "||" + res[i].stockquanity+ "\n");
+
+    }
+    promptCustomer(res);
+  })
+
 }
-customerOrder(res);
-})
 
-
- }
-
-var customerOrder = function (res){
-inquirer
-       .prompt([{
-        type: "input",
-        name: "name",
-        message: "What would you like to buy? "
-    
-       }])
-       .then(function(answer){
-         var correct= false;
-         for(var i=0; i<res.legth; i++){
-          if (res[i].productname==answer.choice) {
-            correct=true;
-            var product=answer.choice;
-            var id=i;
+var promptCustomer = function (res) {
+  inquirer.prompt([{
+    type: 'input',
+    name: 'choice',
+    message: "what do you want to buy? "
+  }]).then(function (answer) {
+    var correct = false;
+    for (var i = 0; i < res.length; i++) {
+      if (res[i].productname== answer.choice) {
+        correct = true;
+        var product = answer.choice;
+        var id = i;
+        inquirer.prompt({
+          type: 'input',
+          name: 'quant',
+          message: "How many dude?  ",
+          validate: function (value) {
+            if (isNaN(value) == false) {
+              return true;
+            } else {
+              return false;
+            }
           }
-         }
+        }).then(function (answer){
+          if((res[id].stockquanity-answer.quant)>0){
+            connection.query("UPDATE products SET stockquanity='"+(res[id].stockquanity-answer.quant)+ "'+ WHERE productname='"+product+"'",function (err, res2){
+            console.log("You got it bro!");
+                purchaseTable()
+
+              })
+
+          } else {
+            console.log("What dude? Just pick something");
+            promptCustomer(res);
+          }
         })
       }
+     }
+  })
+}
 
-//  .then(function(user) {
 
-  
 
-//   console.log(user.name);
-  
-//     })
-  
 
-//take in user input 
 
-//compare to mysql databas = stockqunaity 
 
-//
 
-// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+
+
