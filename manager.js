a66abd66a30aaae2f3afd6237d8c413f60ca36e8
Kaloli <rvellark@gmail.com>
2019-02-21 11:38:38 -0700

    update with manger first one

---
+++ manager.js
@@ -0,0 +1,58 @@
+require ('dotenv').config()
+var mysql = require("mysql");
+var inquirer = require('inquirer');
+var console_table = require('console.table')
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // port
+  port: 3306,
+
+  // username
+  user: "root",
+
+  // password
+  password: process.env.password,
+  database: "bamazonDB"
+})
+
+connection.connect(function (err) {
+  if (err) throw err;
+  
+
+
+})
+
+
+
+
+var purchaseTable = function () {
+    connection.query("SELECT * FROM products", function (err, res) {
+      if (err) throw err
+      console.table(res)
+     
+    
+    })
+  
+  }
+
+    inquirer
+      .prompt({
+        name: "manager",
+        type: "list",
+        message: "hello sir, how can I help you?",
+        choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory","Add New Product"]
+      
+    })
+    .then(function(answer) {
+        // based on their answer, either call the bid or the post functions
+        if (answer.manager === "View Products for Sale") {
+            
+            purchaseTable()
+          
+        }
+
+    
+    })
+   
